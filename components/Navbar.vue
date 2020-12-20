507c647880e4ed0efe05cf966156c97482fa19cc
tatyana <tatyanabak@mail.ru>
2020-12-21 00:41:23 +0700

    убраны лишние элементы из Navbar-а

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,12 +1,8 @@
 <template>
-  <b-navbar toggleable="lg" type="dark" variant="dark" class="mb-3">
-    <b-navbar-brand href="/">AnyCompanyName</b-navbar-brand>
+  <b-navbar toggleable="sm" type="dark" variant="dark" class="mb-3">
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav class="ml-auto">
-        <b-nav-item href="/" class="active">
-          Cписок пользователей
-        </b-nav-item>
         <b-button>Войти</b-button>
       </b-navbar-nav>
     </b-collapse>
